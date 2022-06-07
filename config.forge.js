5fa589175a48b3c08e2e9e282e31f13840cd2e80
ViavaCos <13811092694@163.com>
2022-06-07 14:50:31 +0800

    feat:inject forge config

---
+++ config.forge.js
@@ -0,0 +1,28 @@
+module.exports = {
+  "packagerConfig": {
+    "icon": "./build/favicon.ico",
+    "osxSign": true
+  },
+  "makers": [
+    {
+      "name": "@electron-forge/maker-squirrel",
+      "config": {
+        "name": "my_electron_app"
+      }
+    },
+    {
+      "name": "@electron-forge/maker-zip",
+      "platforms": [
+        "darwin"
+      ]
+    },
+    {
+      "name": "@electron-forge/maker-deb",
+      "config": {}
+    },
+    {
+      "name": "@electron-forge/maker-rpm",
+      "config": {}
+    }
+  ]
+}
