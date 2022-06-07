f64e699c516e0415885d17f23c1b7db33d0dbba5
ViavaCos <13811092694@163.com>
2022-06-07 17:08:40 +0800

    feat:add test file

--- package/index.js
+++ package/index.js
@@ -1,24 +1,24 @@
-const { app, BrowserWindow, ipcMain, Menu }  = require('electron')
+const { app, BrowserWindow, ipcMain, Menu, Notification }  = require('electron')
 const path = require('path')
 
 function createWindow () {
   const win = new BrowserWindow({
-    width: 900,
-    height: 600,
+    width: 900, // 窗口宽度
+    height: 600, // 窗口高度
     // minWidth: 901,
     // minHeight: 601,
     transparent: true, // 透明边框
     webPreferences: { // 开启网页功能设置
       nodeIntegration: true, //是否完整的支持 node. 默认值为true
       nodeIntegrationInWorker: false,// 是否在Web工作器中启用了Node集成
-      preload: path.join(__dirname, 'preload.js')
+      preload: path.join(__dirname, 'preload.js') // 预加载的js文件
     },
     resizable: false,
     // useContentSize: true,
     // backgroundColor: '#2e2c29', // 背景颜色
     opacity: 0.9, // 窗体透明度 0~1
     icon: './icon.png', // 应用图标
-    frame: false // 设置无边框窗口（无工具栏、边框、其它图形化外壳）
+    frame: true // 设置无边框窗口（无工具栏、边框、其它图形化外壳）
   })
 
   // 监听关闭事件
@@ -37,8 +37,9 @@ function createWindow () {
   // 设置无工具栏
   // Menu.setApplicationMenu(null)
 
-  win.loadFile('build/index.html')
-  // win.loadFile('index.html')
+  win.loadFile('build/index.html') // 正式
+  // win.loadFile('test.html') // 直接报错 `Not allowed to load local resource` 就很离谱
+  // win.loadFile('package/test.html') // 测试专用
 
   // win.webContents.on('did-finish-load', () => {
   //   win.webContents.send('ping', { text: 'This is a text.'})
@@ -61,6 +62,12 @@ function createWindow () {
 app.whenReady().then(() => {
   console.log('start createWindow...')
   createWindow()
+
+  // 据说是解决win10的notification问题, 目前用不到
+  // console.log(process.execPath)
+  // if(process.platform === 'win32') {
+  //   app.setAppUserModelId(process.execPath)
+  // }
 }).then(() => {
   console.log('start showNotification...')
   // showNotification()
@@ -70,6 +77,11 @@ app.whenReady().then(() => {
   //   title: '测试弹窗',
   //   message: '这是message',
   //   buttonLabel: '确认label'
+  // })
+
+  // require('electron').dialog.showMessageBox({
+  //   title: '标题',
+  //   message: '内容'
   // })
 })
 
