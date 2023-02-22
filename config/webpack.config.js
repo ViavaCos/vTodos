a69ac1b0dd64b990bebb96437efd6611a12e2076
zhongshitao <1292206925@qq.com>
2023-02-22 20:00:28 +0800

    新增qiankun子应用配置

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -29,6 +29,7 @@ const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin'
 const postcssNormalize = require('postcss-normalize');
 
 const appPackageJson = require(paths.appPackageJson);
+const packageName = require('./package.json').name;
 
 // Source maps are resource heavy and can cause out of memory issue for large source files.
 const shouldUseSourceMap = process.env.GENERATE_SOURCEMAP !== 'false';
@@ -197,6 +198,8 @@ module.exports = function (webpackEnv) {
           ]
         : paths.appIndexJs,
     output: {
+      library: `${packageName}-[name]`,
+      libraryTarget: 'umd',
       // The build folder.
       path: isEnvProduction ? paths.appBuild : undefined,
       // Add /* filename */ comments to generated require()s in the output.
